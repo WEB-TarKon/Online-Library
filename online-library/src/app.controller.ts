9987d6bb474bb50cdb8cd9dc66e7f05d3f1a6722
Alina Taranenko <taranenko.alina2004@gmail.com>
2025-03-05 00:19:42 +0200

    Сntroller setup process

--- online-library/src/app.controller.ts
+++ online-library/src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import {Request, Response} from "express";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  getHello(
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    return res
   }
 }
